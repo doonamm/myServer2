11d11d49a635e53c9df716fca38ef25d2d0ed47e
CuCaRot <namkienlua@gmail.com>
2021-09-27 09:06:54 +0700

    2nd

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const EXPRESS = require('express');
 const APP = EXPRESS();
-const SERVER = require('http').Server(APP);
+const SERVER = require('http').createServer(APP);
 const IO = require('socket.io')(SERVER);
 
 //server
@@ -12,71 +12,156 @@ SERVER.listen(process.env.PORT || 2000, ()=>console.log('\nServer started, liste
 
 //main
 const PLAYER_LIST = {};
+const PLAYER_INIT = [];
 
-IO.on('connection', function(socket){
+IO.sockets.on('connection', function(socket){
     const id = RandomIDSocket();
     console.log('Id:', id, '- connected');
 
-    PLAYER_LIST[id] = Player(id);
+    PLAYER_LIST[id] = new Player(NewPlayerData(id));
+
+    PLAYER_INIT.push({
+        id: id,
+        w: PLAYER_LIST[id].w,
+        color: PLAYER_LIST[id].color
+    });
+
+    IO.sockets.emit('connected', {players: PLAYER_INIT});
 
     socket.on('disconnect', function(){
-        console.log('ID:', id, '- disconnected');
+        RemovePlayer(id);
         delete socket;
-        delete PLAYER_LIST[id];
     });
 
     socket.on('handleInput', function(data){
-        if(data.type === 'keydown'){
-            PLAYER_LIST[id][data.state] = true;
+        if(data.type == 'keydown'){
+            PLAYER_LIST[id].status[data.state] = true;
         }
-        else if(data.type === 'keyup'){
-            PLAYER_LIST[id][data.state] = false;
+        else if(data.type == 'keyup'){
+            PLAYER_LIST[id].status[data.state] = false;
         }
-    })
+    });
 });
 
-setInterval(ClientUpdate, 25);
+setInterval(ClientUpdate, 15);
 
 //function
 function ClientUpdate(){
+    const updatePack = {
+        players: []
+    };
     for(const id in PLAYER_LIST){
-        if(PLAYER_LIST[id].left){
-            PLAYER_LIST[id].x -= PLAYER_LIST[id].speed;
-        }
-        else if(PLAYER_LIST[id].right){
-            PLAYER_LIST[id].x += PLAYER_LIST[id].speed;
-        }
-        if(PLAYER_LIST[id].up){
-            PLAYER_LIST[id].y -= PLAYER_LIST[id].speed;
-        }
-        else if(PLAYER_LIST[id].down){
-            PLAYER_LIST[id].y += PLAYER_LIST[id].speed;
-        }
+        PLAYER_LIST[id].updatePosition();
+        updatePack.players.push(PLAYER_LIST[id].getPack());
     }
 
-    IO.emit('update', PLAYER_LIST);
+    IO.emit('update', updatePack);
 }
 
-function Player(id){
-    return{
-        id: id,
-        x: 250,
-        y: 250,
-        w: 30,
-        h: 30,
-        speed: 10,
-        color: RandomColor(),
+function RemovePlayer(id){
+    console.log('Id:', id, '- disconnected');
+    IO.sockets.emit('disconnected', id);
+
+    PLAYER_INIT.forEach((player, i)=>{
+        if(player.id === id)
+            delete PLAYER_INIT[i];
+    });
+
+    delete PLAYER_LIST[id];
+}
+
+function Player(data){
+    GameObject.call(this, data);
+    this.status = {
         left: false,
         right: false,
         up: false,
         down: false
     }
+    this.getInitialPack = ()=>{
+        return{
+            id: this.id,
+            w: this.w,
+            color: this.color
+        }
+    }
+    this.getPack = ()=>{
+        return{
+            id: this.id,
+            x: this.x,
+            y: this.y,
+        }
+    }
+    this.updatePosition = ()=>{
+        //if object is disabled, do nothing
+        if(this.speed === 0)
+            return;
+
+        if(this.status.left){
+            this.spX = -this.speed;
+        }
+        else if(this.status.right){
+            this.spX = this.speed;
+        }
+        else{
+            this.spX = 0;
+        }
+
+        if(this.status.up){
+            this.spY = - this.speed;
+        }
+        else if(this.status.down){
+            this.spY = this.speed;
+        }
+        else{
+            this.spY = 0;
+        }
+
+        this.x += this.spX;
+        this.y += this.spY;
+
+        this.checkCollision();
+    }
+    this.checkCollision = ()=>{
+        const del = this.w/2;
+        if(this.x - del < 0)
+            this.x = del;
+        else if(this.x + del > 500)
+            this.x = 500 - del;
+        if(this.y - del < 0)
+            this.y = del;
+        else if(this.y + del > 500)
+            this.y = 500 - del;
+    }
+}
+
+function GameObject(data){
+    this.id = data.id;
+    this.x = data.x || 250;
+    this.y = data.y || 250;
+    this.w = data.w || 10;
+    this.h = data.h || 10;
+    this.speed = data.speed || 0;
+    this.spX = data.spX || 0;
+    this.spY = data.spY || 0;
+    this.color = data.color || 'black';
+}
+
+function NewPlayerData(id){
+    return{
+        id: id,
+        x: 250,
+        y: 250,
+        w: 20,
+        speed: 10,
+        color: RandomColor()
+    }
 }
 
 function RandomColor(){
-    const r = RandomInRange(0, 255);
-    const g = RandomInRange(0, 255);
-    const b = RandomInRange(0, 255);
+    const r = RandomInRange(22, 222);
+    const g = RandomInRange(22, 222);
+    const b = RandomInRange(22, 222);
     return '#' + r.toString(16) + g.toString(16) + b.toString(16);
 }
 
--- client/script.js
+++ client/script.js
@@ -10,26 +10,49 @@ const App = ()=>{
     const canvas_w = 500;
     const canvas_h = 500;
     const ctx = canvas.getContext('2d');
+    const PLAYER_OFFSET = {};
 
-    socket.on('update', (data)=>{
+    socket.on('connected', OnConnected);
+    socket.on('disconnected', OnDisconnected);
+    socket.on('update', Update);
+
+    // ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise)
+    function Update(data){
         ctx.clearRect(0, 0, canvas_w, canvas_h);
-        for(let id in data){
-            ctx.fillStyle = data[id].color;
-            ctx.fillRect(data[id].x, data[id].y, data[id].w, data[id].h);
+        for(let player of data.players){
+            if(player){
+                const {w, color} = PLAYER_OFFSET[player.id];
+                ctx.fillStyle = color;
+                ctx.fillRect(player.x - w/2, player.y - w/2, w, w);
+            }
         }
-    });
-    
-    function Update(){
-        ctx.clearRect(0, 0, canvas_w, canvas_h);
+    }
+
+    function OnConnected(data){
+        for(let player of data.players){
+            if(player){
+                PLAYER_OFFSET[player.id] = {
+                    w: player.w,
+                    color: player.color
+                }
+            }
+        }
+        console.log(Object.keys(PLAYER_OFFSET), Object.keys(PLAYER_OFFSET).length);
+    }
+
+    function OnDisconnected(id){
+        delete PLAYER_OFFSET[id];
+        console.log(Object.keys(PLAYER_OFFSET), Object.keys(PLAYER_OFFSET).length);
     }
 
     document.addEventListener('keydown', (e)=>{
+        e.preventDefault();
         switch(e.code){
             case 'KeyA':
             case 'KeyD':
             case 'KeyW':
             case 'KeyS':
-                socket.emit('handleInput', {
+                socket.volatile.emit('handleInput', {
                     type: 'keydown',
                     state: DIRECTION[e.code]
                 });
@@ -42,7 +65,7 @@ const App = ()=>{
             case 'KeyD':
             case 'KeyW':
             case 'KeyS':
-                socket.emit('handleInput', {
+                socket.volatile.emit('handleInput', {
                     type: 'keyup',
                     state: DIRECTION[e.code]
                 });
