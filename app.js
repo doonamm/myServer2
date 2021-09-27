a530b1c49c45194429315b11dc4cc0e9e20e8451
CuCaRot <namkienlua@gmail.com>
2021-09-27 20:00:50 +0700

    2nd

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ SERVER.listen(process.env.PORT || 2000, ()=>console.log('\nServer started, liste
 const PLAYER_LIST = {};
 const PLAYER_INIT = [];
 
+const BOMP_LIST = {};
+const BOMP_INIT = [];
+const NEW_BOMP = [];
+
+const updatePack = {
+    players: [],
+    bomps: []
+};
+
+const deletePack = {
+    bomps: []
+}
+
+
 IO.sockets.on('connection', function(socket){
     const id = RandomIDSocket();
     console.log('Id:', id, '- connected');
@@ -26,7 +40,10 @@ IO.sockets.on('connection', function(socket){
         color: PLAYER_LIST[id].color
     });
 
-    IO.sockets.emit('connected', {players: PLAYER_INIT});
+    IO.sockets.emit('connected', {
+        players: PLAYER_INIT,
+        bomps: BOMP_INIT
+    });
 
     socket.on('disconnect', function(){
         RemovePlayer(id);
@@ -41,42 +58,190 @@ IO.sockets.on('connection', function(socket){
             PLAYER_LIST[id].status[data.state] = false;
         }
     });
+
+    socket.on('mousemove', (data)=>{
+        PLAYER_LIST[id].aim = data;
+    })
 });
 
 setInterval(ClientUpdate, 15);
 
 //function
 function ClientUpdate(){
-    const updatePack = {
-        players: []
-    };
     for(const id in PLAYER_LIST){
-        PLAYER_LIST[id].updatePosition();
         updatePack.players.push(PLAYER_LIST[id].getPack());
+        PLAYER_LIST[id].update();
+
+        const {w, x, y} = PLAYER_LIST[id];
+        for(const id_bomp in BOMP_LIST){
+            BOMP_LIST[id_bomp].checkCollideObject(id, x, y, w);
+        }
+    }
+    for(const id in BOMP_LIST){
+        updatePack.bomps.push(BOMP_LIST[id].getPack());
+        BOMP_LIST[id].update();
     }
 
-    IO.emit('update', updatePack);
-}
-
-function RemovePlayer(id){
-    console.log('Id:', id, '- disconnected');
-    IO.sockets.emit('disconnected', id);
-
-    PLAYER_INIT.forEach((player, i)=>{
-        if(player.id === id)
-            delete PLAYER_INIT[i];
+    IO.emit('update', {
+        initBomps: NEW_BOMP,
+        update: updatePack,
+        delete: deletePack
     });
 
-    delete PLAYER_LIST[id];
+    updatePack.players = [];
+    updatePack.bomps = [];
+    deletePack.bomps = [];
+    NEW_BOMP.length = 0;;
+}
+
+function Bomb(data){
+    GameObject.call(this, data);
+    this.r = data.r;
+    this.followObjectId = data.followObjectId || null;
+    this.shooterId = data.shooterId;
+    this.existTime = data.existTime;
+    this.countDown = -1;
+    this.maxDistance = data.maxDistance;
+    this.currentDistance = 0;
+    this.spX =  Math.round(Math.cos(data.angle) * this.speed);
+    this.spY =  Math.round(Math.sin(data.angle) * this.speed);
+
+    this.getInitialPack = ()=>{
+        return{
+            id: this.id,
+            w: this.w,
+            color: this.color,
+            r: this.r,
+        }
+    }
+    this.getPack = ()=>{
+        return{
+            id: this.id,
+            x: this.x,
+            y: this.y
+        }
+    }
+    this.checkCollideObject = (id, x, y, w)=>{
+        if(this.followObjectId !== null || this.shooterId == id || this.speed == 0)
+            return;
+        const distance = GetDistance({x: this.x, y: this.y}, {x, y})
+        if(distance <= this.w + w){
+            this.followObjectId = id;
+            this.countDown = this.existTime;
+            PLAYER_LIST[id].stickyObject.push(this.id);
+        }
+    }
+    this.checkExplodeArea = (id, x, y, w)=>{
+        const distance = GetDistance({x: this.x, y: this.y}, {x, y});
+        if(distance <= this.r + w){
+            PLAYER_LIST[id].reset();
+        }
+    }
+    this.removeBomb = ()=>{
+        if(this.followObjectId !== null)
+            PLAYER_LIST[this.followObjectId].removeStickyObject(this.id);
+        for(const id_player in PLAYER_LIST){
+            const {w, x, y} = PLAYER_LIST[id_player];
+            this.checkExplodeArea(id_player, x, y, w);
+        }
+        BOMP_INIT.forEach((bomp, i)=>{
+            if(bomp.id === this.id)
+                delete BOMP_INIT[i];
+        });
+        deletePack.bomps.push(this.id);
+        delete BOMP_LIST[this.id];
+    }
+    this.updatePosition = ()=>{
+        if(this.speed == 0)
+            return;
+        if(this.followObjectId !== null){
+            if(PLAYER_LIST[this.followObjectId] !== undefined){
+                this.x = PLAYER_LIST[this.followObjectId].x;
+                this.y = PLAYER_LIST[this.followObjectId].y;
+            }
+            else{
+                this.speed = 0;
+                this.followObjectId = null;
+            }
+        }
+        else{
+            this.x += this.spX;
+            this.y += this.spY;
+
+            this.currentDistance += this.speed;
+            if(this.checkCollision() == true || this.currentDistance >= this.maxDistance){
+                this.speed = 0;
+                this.countDown = this.existTime;
+            }
+        }
+    }
+    this.counting = ()=>{
+        if(this.countDown > 0){
+            this.countDown--;
+            if(this.countDown <= 0){
+                this.removeBomb();
+            }
+        }
+    }
+    this.checkCollision = ()=>{
+        let isCollide = false;
+        if(this.x < 0){
+            this.x = 0;
+            isCollide = true;
+        }
+        else if(this.x > 500){
+            this.x = 500;
+            isCollide = true;
+        }
+        else if(this.y < 0){
+            this.y = 0;
+            isCollide = true;
+        }
+        else if(this.y > 500){
+            this.y = 500;
+            isCollide = true;
+        }
+        if(isCollide){
+            this.speed = 0;
+            this.countDown = this.existTime;
+        }
+    }
+    this.update = ()=>{
+        this.updatePosition();
+        this.checkCollision();
+        this.counting();
+    }
+}
+
+function NewBombData({shooterId, x, y, color, angle}){
+    return{
+        id: RandomInRange(0, 999999),
+        x: x,
+        y: y,
+        w: 8,
+        color: color,
+        speed: 15,
+        angle: angle,
+        shooterId: shooterId,
+        r: 30,
+        maxDistance: 300,
+        existTime: 100
+    }
 }
 
 function Player(data){
     GameObject.call(this, data);
+    this.stickyObject = [];
+    this.aim = {
+        x: 0,
+        y: 0
+    }
     this.status = {
         left: false,
         right: false,
         up: false,
-        down: false
+        down: false,
+        shoot: false
     }
     this.getInitialPack = ()=>{
         return{
@@ -90,6 +255,32 @@ function Player(data){
             id: this.id,
             x: this.x,
             y: this.y,
+        }
+    }
+    this.update = ()=>{
+        this.updatePosition();
+        this.checkCollision();
+        this.shoot();
+    }
+    this.shoot = ()=>{ 
+        if(this.status.shoot){
+            this.status.shoot = false;
+            const bomp = new Bomb(NewBombData({
+                shooterId: this.id,
+                x: this.x,
+                y: this.y,
+                color: this.color,
+                angle: Math.atan2(this.aim.y - this.y, this.aim.x - this.x)
+            }));
+            BOMP_LIST[bomp.id] = bomp;
+            const init_data = {
+                id: bomp.id,
+                w: bomp.w,
+                color: bomp.color,
+                r: bomp.r
+            };
+            BOMP_INIT.push(init_data);
+            NEW_BOMP.push(init_data)
         }
     }
     this.updatePosition = ()=>{
@@ -119,8 +310,6 @@ function Player(data){
 
         this.x += this.spX;
         this.y += this.spY;
-
-        this.checkCollision();
     }
     this.checkCollision = ()=>{
         const del = this.w/2;
@@ -132,6 +321,20 @@ function Player(data){
             this.y = del;
         else if(this.y + del > 500)
             this.y = 500 - del;
+    }
+    this.reset = () =>{
+        this.x = 250;
+        this.y = 250;
+        for(const id of this.stickyObject){
+            BOMP_LIST[id].speed = 0;
+        }
+    }
+    this.removeStickyObject = (id_bomp)=>{
+        this.stickyObject.forEach((id, i) => {
+            if(id_bomp == id){
+                this.stickyObject.splice(i, 1);
+            }
+        });
     }
 }
 
@@ -145,6 +348,24 @@ function GameObject(data){
     this.spX = data.spX || 0;
     this.spY = data.spY || 0;
     this.color = data.color || 'black';
+}
+
+function GetDistance(pos1, pos2){
+    const delX = pos1.x - pos2.x;
+    const delY = pos1.y - pos2.y;
+    return Math.round(Math.sqrt(delX * delX + delY * delY));
+}
+
+function RemovePlayer(id){
+    console.log('Id:', id, '- disconnected');
+    IO.sockets.emit('disconnected', id);
+
+    PLAYER_INIT.forEach((player, i)=>{
+        if(player.id === id)
+            delete PLAYER_INIT[i];
+    });
+
+    delete PLAYER_LIST[id];
 }
 
 function NewPlayerData(id){
--- client/script.js
+++ client/script.js
@@ -3,14 +3,22 @@ const DIRECTION = {
     'KeyA': 'left',
     'KeyD': 'right',
     'KeyW': 'up',
-    'KeyS': 'down'
+    'KeyS': 'down',
+    'ArrowLeft': 'left',
+    'ArrowRight': 'right',
+    'ArrowUp': 'up',
+    'ArrowDown': 'down',
+    'Space': 'shoot'
 }
+const PLAYER_OFFSET = {};
+const BOMP_OFFSET = {};
+const MINCOUNT = 10;
+
 const App = ()=>{
     const canvas = document.getElementById('cvs');
     const canvas_w = 500;
     const canvas_h = 500;
     const ctx = canvas.getContext('2d');
-    const PLAYER_OFFSET = {};
 
     socket.on('connected', OnConnected);
     socket.on('disconnected', OnDisconnected);
@@ -19,17 +27,61 @@ const App = ()=>{
     // ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise)
     function Update(data){
         ctx.clearRect(0, 0, canvas_w, canvas_h);
-        for(let player of data.players){
+        for(const player of data.update.players){
             if(player){
                 const {w, color} = PLAYER_OFFSET[player.id];
                 ctx.fillStyle = color;
                 ctx.fillRect(player.x - w/2, player.y - w/2, w, w);
             }
         }
+        for(const new_bomp of data.initBomps){
+            BOMP_OFFSET[new_bomp.id] = {
+                w: new_bomp.w,
+                color: new_bomp.color,
+                r: new_bomp.r,
+                count: MINCOUNT,
+                time: 0
+            }
+        }
+        for(const bomp of data.update.bomps){
+            if(bomp){
+                const {w, r, color, count, time} = BOMP_OFFSET[bomp.id];
+                ctx.fillStyle = color;
+                //border
+                ctx.beginPath();
+                ctx.globalAlpha = 0.2;
+                ctx.arc(bomp.x, bomp.y, r, 0, 2*Math.PI);
+                ctx.stroke();
+                //padding
+                ctx.beginPath();
+                ctx.globalAlpha = 0.2;
+                ctx.arc(bomp.x, bomp.y, r, 0, 2*Math.PI);
+                ctx.fill();
+                //effect
+                ctx.beginPath(); 
+                ctx.globalAlpha = 0.4;
+                ctx.arc(bomp.x, bomp.y, r * count / 100, 0, 2*Math.PI);
+                if(time % 2 == 0)
+                    BOMP_OFFSET[bomp.id].count+= 10;
+                if(count > 100)
+                    BOMP_OFFSET[bomp.id].count = MINCOUNT;
+                BOMP_OFFSET[bomp.id].time++;
+                ctx.fill();
+                ctx.globalAlpha = 1;
+                //center
+                ctx.beginPath();
+                ctx.arc(bomp.x, bomp.y, w, 0, 2*Math.PI);
+                ctx.fill();
+            }
+        }
+        for(const id of data.delete.bomps){
+            if(BOMP_OFFSET[id])
+                delete BOMP_OFFSET[id];
+        }
     }
 
     function OnConnected(data){
-        for(let player of data.players){
+        for(const player of data.players){
             if(player){
                 PLAYER_OFFSET[player.id] = {
                     w: player.w,
@@ -37,7 +89,18 @@ const App = ()=>{
                 }
             }
         }
-        console.log(Object.keys(PLAYER_OFFSET), Object.keys(PLAYER_OFFSET).length);
+        for(const bomp of data.bomps){
+            if(bomp){
+                BOMP_OFFSET[bomp.id] = {
+                    w: bomp.w,
+                    color: bomp.color,
+                    r: bomp.r,
+                    count: MINCOUNT,
+                    time: 0
+                }
+            }
+        }
+        console.log(Object.keys(BOMP_OFFSET), Object.keys(BOMP_OFFSET).length);
     }
 
     function OnDisconnected(id){
@@ -52,6 +115,11 @@ const App = ()=>{
             case 'KeyD':
             case 'KeyW':
             case 'KeyS':
+            case 'Space':
+            case 'ArrowLeft':
+            case 'ArrowRight':
+            case 'ArrowUp':
+            case 'ArrowDown':
                 socket.volatile.emit('handleInput', {
                     type: 'keydown',
                     state: DIRECTION[e.code]
@@ -65,6 +133,10 @@ const App = ()=>{
             case 'KeyD':
             case 'KeyW':
             case 'KeyS':
+            case 'ArrowLeft':
+            case 'ArrowRight':
+            case 'ArrowUp':
+            case 'ArrowDown':
                 socket.volatile.emit('handleInput', {
                     type: 'keyup',
                     state: DIRECTION[e.code]
@@ -72,5 +144,18 @@ const App = ()=>{
                 break;
         }
     });
+    document.addEventListener('mousemove', (e)=>{
+        e.preventDefault();
+        const {clientX: x, clientY: y} = e;
+        socket.volatile.emit('mousemove', {x, y});
+    })
+    document.addEventListener('mousedown', (e)=>{
+        e.preventDefault();
+        socket.volatile.emit('handleInput', {
+            type: 'keydown',
+            state: 'shoot'
+        });
+    });
+    document.addEventListener('contextmenu', (e)=>e.preventDefault());
 }
 window.addEventListener('load', App);
